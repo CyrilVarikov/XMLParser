7094ac05f5a652da348b999c40fe1e14e07ed79b
CyrilVarikov <cyril.varikov@gmail.com>
2018-05-11 00:17:47 +0300

    implemetation of task

---
+++ XmlParser/XmlParser/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace XmlParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_open = new System.Windows.Forms.Button();
+            this.rtb_roles = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(677, 391);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(111, 47);
+            this.btn_open.TabIndex = 0;
+            this.btn_open.Text = "Open";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // rtb_roles
+            // 
+            this.rtb_roles.Location = new System.Drawing.Point(1, 6);
+            this.rtb_roles.Name = "rtb_roles";
+            this.rtb_roles.ReadOnly = true;
+            this.rtb_roles.Size = new System.Drawing.Size(647, 432);
+            this.rtb_roles.TabIndex = 1;
+            this.rtb_roles.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtb_roles);
+            this.Controls.Add(this.btn_open);
+            this.Name = "Form1";
+            this.Text = "XMLParser";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.RichTextBox rtb_roles;
+    }
+}
+
---
+++ XmlParser/XmlParser/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XmlParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.DefaultExt = ".xml";
+            openFileDialog.Filter = "Text documents (.xml)|*.xml";
+
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+                using (XmlTextReader reader = new XmlTextReader(openFileDialog.OpenFile()))
+                {
+                    
+                    while (reader.Read())
+                    {
+                        if (reader.NodeType == XmlNodeType.Element)
+                        {
+                            if (reader.HasAttributes)
+                            {
+                                string key = reader.GetAttribute(0);
+                                if (!keyValuePairs.ContainsKey(key))
+                                {
+                                    keyValuePairs.Add(key, 1);
+                                }
+                                else
+                                {
+                                    int value = keyValuePairs[key];
+                                    keyValuePairs[key] = ++keyValuePairs[key];
+                                }
+
+                            }
+                            
+                        }
+                    }
+                }
+                foreach(KeyValuePair<string, int> entry in keyValuePairs)
+                {
+                    rtb_roles.Text += entry.Key + " : " + entry.Value + "\r\n";
+                }
+                Cursor.Current = Cursors.Default;
+            }
+        }
+    }
+}
